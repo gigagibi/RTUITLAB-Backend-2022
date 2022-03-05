91bd2bed8a5bf6426a317c5289d9bf008f594458
gigagibi <gii01@mail.ru>
2022-03-06 02:28:46 +0300

    (orders) added tests for mappers

--- orders/src/test/java/rtuitlab/orders/OrdersApplicationTests.java
+++ orders/src/test/java/rtuitlab/orders/OrdersApplicationTests.java
@@ -2,8 +2,14 @@ package rtuitlab.orders;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.ComponentScan;
+import rtuitlab.orders.mappers.DeliveryOrderMapper;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
+@SpringBootTest
 public class OrdersApplicationTests {
+    public static void main(String[] args) {
+
+    }
 }
