fa0717b20798cbc3650bb8938a3f1530bc4010b1
gigagibi <gii01@mail.ru>
2022-03-17 12:24:57 +0300

    added deliveries to docker-compose + small fixes

--- deliveries/src/main/java/rtuitlab/deliveries/config/rabbit/RabbitConfiguration.java
+++ deliveries/src/main/java/rtuitlab/deliveries/config/rabbit/RabbitConfiguration.java
@@ -6,8 +6,6 @@ import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitAdmin;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
-import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
-import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -21,11 +19,6 @@ public class RabbitConfiguration {
     @Bean
     public ConnectionFactory connectionFactory() {
         return new CachingConnectionFactory(host);
-    }
-
-    @Bean
-    public Jackson2JsonMessageConverter jackson2MessageConverter() {
-        return new Jackson2JsonMessageConverter();
     }
 
     @Bean
--- products/src/main/java/rtuitlab/products/config/rabbit/RabbitConfiguration.java
+++ products/src/main/java/rtuitlab/products/config/rabbit/RabbitConfiguration.java
@@ -10,7 +10,9 @@ import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class RabbitConfiguration {
     @Value("${rabbitmq.host}")
     private String host;
@@ -18,11 +20,6 @@ public class RabbitConfiguration {
     @Bean
     public ConnectionFactory connectionFactory() {
         return new CachingConnectionFactory(host);
-    }
-
-    @Bean
-    public Jackson2JsonMessageConverter jackson2MessageConverter() {
-        return new Jackson2JsonMessageConverter();
     }
 
     @Bean
